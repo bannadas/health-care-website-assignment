37c69a689dbab9a34843bd8289c2eb906231d954
bannadas <bannadaso39@gmail.com>
2021-10-18 13:18:02 +0600

    second commit

--- src/ServiceDetail/ServiceDetail.js
+++ src/ServiceDetail/ServiceDetail.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { useParams } from 'react-router';
 import NavigationBar from '../pages/NavigationBar/NavigationBar';
 
 const ServiceDetail = () => {
-    const {friendId} = useParams();
-   
+    const {serviceId} = useParams();
+  const [serviceDetails, setServiceDetails] = useState([]);
+  useEffect(() => {
+      fetch('/servicedetail.json')
+      .then(res => res.json())
+      .then(data => setServiceDetails(data.service))
+  },[])
+
+useEffect(()=> {
+   const findService =  serviceDetails.find(service => service.login.id === serviceId)
+    console.log(findService);
+},[serviceDetails])
     return (
         <div>
             <NavigationBar></NavigationBar>
+            <h1>{serviceId}</h1>
             <h1> Detail</h1>
         </div>
     );
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -5,7 +5,7 @@ import './Services.css'
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('./data.json')
+        fetch('/data.json')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
