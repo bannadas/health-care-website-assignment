fbd7478c0ff2179a02ede0bfb066c05327f29c1a
bannadas <bannadaso39@gmail.com>
2021-10-19 23:30:58 +0600

    doctor details started

--- src/pages/DoctorDetails/DoctorDetails.js
+++ src/pages/DoctorDetails/DoctorDetails.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavigationBar from '../NavigationBar/NavigationBar';
+import './DoctorDetails.css'
 
 const DoctorDetails = () => {
+    const [details, setDetials] = useState([]);
+    useEffect(() => {
+        fetch('/Doctordetail.json')
+            .then(res => res.json())
+            .then(data => setDetials(data));
+    }, [])
     return (
         <div>
             <NavigationBar></NavigationBar>
-            <h1>Doctors</h1>
+            <h1 className="text-center">All Our Specialist Doctors</h1>
+            <div className="container cards">
+                {details.map(detail =>
+                     <div >
+                       <h2> {detail.name}</h2>
+                       
+                     </div>
+                     )}
+            </div>
         </div>
     );
 };
