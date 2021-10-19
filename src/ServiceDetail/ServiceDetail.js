13fbfedc597f843b64677623fe352903692c29a0
bannadas <bannadaso39@gmail.com>
2021-10-19 16:34:47 +0600

    slider added

--- src/ServiceDetail/ServiceDetail.js
+++ src/ServiceDetail/ServiceDetail.js
@@ -1,16 +1,22 @@
 import React, { useState,useEffect } from 'react';
 import { useParams } from 'react-router';
+import useAuth from '../hooks/useAuth';
 
 import NavigationBar from '../pages/NavigationBar/NavigationBar';
 
 const ServiceDetail = () => {
-    const {serviceId} = useParams();
+    
+    // const {services} = useAuth();
+    // console.log(services);
+    // const exactMaching = service.find(sv => sv.id === Number(serviceId));
+    // const {image,name,id}=exactMaching;
    
     return (
         <div>
             <NavigationBar></NavigationBar>
            
             <h1> Detail</h1>
+            
             
         </div>
     );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import Services from '../../Services/Services';
 import Banner from '../Banner/Banner';
+import './Home.css'
+
+
 
 const Home = () => {
     return (
         <div id="home">
+            
             <Banner></Banner>
             <Services></Services>
+            
+
+            <div className="container my-4  slider">
+            <Carousel fade>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 slider-img"
+      src="https://media.istockphoto.com/photos/offering-patientcentred-care-that-proves-effective-and-efficient-picture-id1301555107?b=1&k=20&m=1301555107&s=170667a&w=0&h=urags5L_xQX4tiAVkW8Hws70_Z2EwnFvDoT2gOqgfk8="
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Uncompromising excellence.Commitment to care </h3>
+      
+      <button>Get Appointment</button>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 slider-img"
+      src="https://media.istockphoto.com/photos/doctors-appointment-picture-id1254899249?b=1&k=20&m=1254899249&s=170667a&w=0&h=c3A0Arr9vqnZ_RiUuQmpKivssqVZ5im84fl_EkrKMD8="
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>World class care right where you need it.</h3>
+      
+      <button>Get Appointment</button>
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+</Carousel>
+            </div>
         </div>
     );
 };
